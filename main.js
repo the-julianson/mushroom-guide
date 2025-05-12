b9255169ad85f1682b26b70331353635d077c419
jbazanaguirre <jbazanaguirre@gmail.com>
2025-05-12 14:13:52 -0300

    Add JS to toggle the menu open

---
+++ main.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector(
+  '[aria-controls="primary-navigation"]'
+);
+
+// const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  const navOpened = navToggle.getAttribute("aria-expanded");
+
+  if (navOpened === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+});
